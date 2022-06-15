8a821c12f4530c95be46917ad854af408117e722
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-06-15 22:23:59 +0700

    HotFix
    - binding for expenditure and reciept windows.

--- FinancialManagerUI/ViewModels/MainWindowViewModel.cs
+++ FinancialManagerUI/ViewModels/MainWindowViewModel.cs
@@ -9,10 +9,10 @@ namespace FinancialManagerUI.ViewModels
 {
     internal class MainWindowViewModel:ViewModelBase
     {
-        #region NameOfSomething
-        private string _Title = "MyWindow";
+        #region NameOfApp
+        private string _Title = "My Purse";
         /// <summary>
-        /// Name of something
+        /// Name of applicATION
         /// </summary>
         public string Title
         {
@@ -21,12 +21,51 @@ namespace FinancialManagerUI.ViewModels
         }
         #endregion
 
-        #region NewCategory
+        #region AddNewCategory
+        /// <summary>
+        /// name of new category for expenditure or reciept
+        /// </summary>
         private string _NewCategory = "";
         public string NewCategory
         {
             get => _NewCategory;
             set => Set(ref _NewCategory, value);
+        }
+        #endregion
+
+        #region MyBudget
+        /// <summary>
+        /// Amount of your budget
+        /// </summary>
+        private string _MyBudget = "0";
+        public string MyBudget
+        {
+            get => _MyBudget;
+            set => Set(ref _MyBudget, value);
+        }
+        #endregion
+
+        #region AmountOfRecieptOrExpenditure
+        /// <summary>
+        /// Amount of your budget
+        /// </summary>
+        private string _AmountOfRecieptOrExpenditure = "0";
+        public string AmountOfRecieptOrExpenditure
+        {
+            get => _AmountOfRecieptOrExpenditure;
+            set => Set(ref _AmountOfRecieptOrExpenditure, value);
+        }
+        #endregion
+
+        #region NoteForRecieptOrExpenditure
+        /// <summary>
+        /// Amount of your budget
+        /// </summary>
+        private string _NoteForRecieptOrExpenditure = "notes";
+        public string NoteForRecieptOrExpenditure
+        {
+            get => _NoteForRecieptOrExpenditure;
+            set => Set(ref _NoteForRecieptOrExpenditure, value);
         }
         #endregion
     }
