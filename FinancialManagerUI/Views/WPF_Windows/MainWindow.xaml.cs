326212f197cac2d2a59102b272e5bd4b1ffb01d7
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-06-17 19:21:03 +0700

    Update MainWindow.xaml.cs

--- FinancialManagerUI/Views/WPF_Windows/MainWindow.xaml.cs
+++ FinancialManagerUI/Views/WPF_Windows/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace FinancialManagerUI
     {
         public MainWindow()
         {
-            BudgetManagerLibrary.GlobalConfig.InitializeConnections(BudgetManagerLibrary.DatabaseType.Sql);//replace Sql to text to check how it work
+            BudgetManagerLibrary.GlobalConfig.InitializeConnections(BudgetManagerLibrary.DatabaseType.Sql);
             InitializeComponent();
         }
         //TODO - value of my purse from database
